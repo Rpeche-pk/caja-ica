30bf7ad9ebf529eb71e5e218a3c2a8723bb1ebc5
Rpeche-pk <pecheaparcana1998@gmail.com>
2024-02-27 22:19:11 -0500

    feat(backend): 🎉init project

---
+++ src/main/java/com/lrpa/app/PokeApiApplication.java
@@ -0,0 +1,13 @@
+package com.lrpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lrpa/app/PokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrpa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
