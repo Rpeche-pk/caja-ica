30c08d340ba4d4e6088911416c1186bc70d62833
Rpeche-pk <pecheaparcana1998@gmail.com>
2024-02-28 19:35:35 -0500

    deploy(pokemon): 🚀 deploy api spring github actions and properties.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS Pokemon (
+    id SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    feeding VARCHAR(50) NOT NULL,
+    size VARCHAR(50) NOT NULL,
+    weight VARCHAR(50) NOT NULL,
+    rarity VARCHAR(50) NOT NULL,
+    fun_fact VARCHAR(250) NOT NULL,
+    enabled boolean DEFAULT true,
+    last_modified_date timestamp not null default current_timestamp,
+    created_date timestamp not null default current_timestamp
+);
+
 INSERT INTO pokemon (name, type, feeding, size, weight, rarity, fun_fact,enabled)
 VALUES ('Pikachu', 'Eléctrico', 'Omnívoro', 'Pequeño', '6.0 kg', 'Común', 'Siempre tiene energía estática en su cuerpo',TRUE);
 INSERT INTO pokemon (name, type, feeding, size, weight, rarity, fun_fact,enabled)
